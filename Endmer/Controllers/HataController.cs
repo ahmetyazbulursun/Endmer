fea6b5d68426fbed3f92b7cb989020a00c80cfe0
Ahmet Hakan Eroğlu <ahmet20033134@gmail.com>
2022-12-08 16:38:07 +0300

    404 hata sayfası ve kontrolü eklendi.

---
+++ Endmer/Controllers/HataController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Endmer.Models.Entity;
+
+namespace Endmer.Controllers
+{
+    public class HataController : Controller
+    {
+
+        EndmerEntities db = new EndmerEntities();
+
+        public ActionResult Hata404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+
+        public ActionResult AnaSayfayaDon()
+        {
+            string userID = Convert.ToString(Session["ID"]);
+            
+            if (userID == "")
+            {
+                Session.Abandon();
+                return RedirectToAction("Logout", "Login");
+            }
+
+            return RedirectToAction("Index", "AnaSayfa");
+        }
+
+
+    }
+}
